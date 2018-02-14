19717f0668aa2585265d5718f9f367462b2d5ff2
Samuel Barrera <sbarrera@empresas.security.cl>
2018-02-14 15:27:10 -0300

    Mas desafios

--- Problem Solving - Algorithms/E006 - Climbing the Leaderboard/ejercicio.py
+++ Problem Solving - Algorithms/E006 - Climbing the Leaderboard/ejercicio.py
@@ -2,30 +2,6 @@
 # >>> https://www.hackerrank.com/challenges/climbing-the-leaderboard/problem
 
 #!/bin/python3
-# V1
-#     if len(scores) == 0:
-#         return [1] * len(alice)
-
-#     retorno = []
-#     totalPuntaje = sorted(list(set(map(int, scores))), reverse=True)
-#     alicePuntaje = list(map(int, alice))
-#     minimo = min(totalPuntaje)
-#     maximo = max(totalPuntaje)
-#     ultimaPosicion = len(totalPuntaje) + 1
-
-#     for elem in [x for x in alicePuntaje if x <= minimo]:
-#         retorno.append(ultimaPosicion)
-#         alicePuntaje.remove(elem)
-    
-#     for elem in [x for x in alicePuntaje if x >= maximo]:
-#         retorno.append(1)
-#         alicePuntaje.remove(elem)
-
-#     for valor in alicePuntaje:
-#         totalPuntaje = [x for x in totalPuntaje if x > valor]
-#         retorno.append(len(totalPuntaje) + 1)
-
-#     return sorted(retorno, reverse=True)
 
 import sys
 
@@ -33,24 +9,32 @@ def climbingLeaderboard(scores, alice):
     if len(scores) == 0:
         return [1] * len(alice)
 
+    if len(alice) == 0:
+        return []
+
     retorno = []
+
     totalPuntaje = sorted(list(set(map(int, scores))), reverse=True)
     alicePuntaje = list(map(int, alice))
     minimo = min(totalPuntaje)
     maximo = max(totalPuntaje)
     ultimaPosicion = len(totalPuntaje) + 1
-    valoresMenores = len([x for x in alicePuntaje if x <= minimo])
-    retorno += [1] * valoresMenores
+
+    valoresMenores = len([x for x in alicePuntaje if x < minimo])
+    retorno += [ultimaPosicion] * valoresMenores
     valoresMayores = len([x for x in alicePuntaje if x >= maximo])
-    retorno += [ultimaPosicion] * valoresMayores
+    retorno += [1] * valoresMayores
     alicePuntaje = alicePuntaje[(valoresMenores):(len(alicePuntaje) - valoresMayores)]
-
+    listaPuntajes = sorted(totalPuntaje)
     for valor in alicePuntaje:
-        totalPuntaje = [x for x in totalPuntaje if x > valor]
-        retorno.append(len(totalPuntaje) + 1)
-
-    return sorted(retorno, reverse=True)
-    # Complete this function
+        contador = 0
+        for elem in listaPuntajes:
+            if valor < elem:
+                listaPuntajes = listaPuntajes[contador:]
+                retorno.append(len(listaPuntajes) + 1)
+                break
+            contador += 1    
+    return sorted(list(retorno), reverse=True)
 
 if __name__ == "__main__":
     n = 7 #int(input().strip())
