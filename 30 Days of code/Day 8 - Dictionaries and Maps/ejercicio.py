b28aa201fc7bae6c0504ad11df70ab478801f3ba
Samuel Barrera <sbarrera@empresas.security.cl>
2018-02-15 14:47:38 -0300

    Mas desafios

--- 30 Days of code/Day 8 - Dictionaries and Maps/ejercicio.py
+++ 30 Days of code/Day 8 - Dictionaries and Maps/ejercicio.py
@@ -11,10 +11,23 @@ if __name__ == "__main__":
     for _ in range(n):
         try:
             nombre = str(input().strip())
-            #resultado = diccionario.get(query, None)
             if nombre in diccionario:
                 print('{}={}'.format(nombre, str(diccionario[nombre][0])))
             else:
                 print('Not found')
         except:
             break
+
+Mejor opcion...
+n = int(input())
+name_numbers = [input().split() for _ in range(n)]
+phone_book = {k: v for k,v in name_numbers}
+while True:
+    try:
+        name = input()
+        if name in phone_book:
+            print('%s=%s' % (name, phone_book[name]))
+        else:
+            print('Not found')
+    except:
+        break
