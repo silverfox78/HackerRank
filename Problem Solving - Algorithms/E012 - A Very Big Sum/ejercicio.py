556f0965b9cf3b85a61f6b61e9069675575ea4c0
Samuel Barrera <sbarrera@gmail.com>
2018-02-24 14:34:08 -0300

    Mas desafios

---
+++ Problem Solving - Algorithms/E012 - A Very Big Sum/ejercicio.py
@@ -0,0 +1,14 @@
+# >>> A Very Big Sum
+# >>> https://www.hackerrank.com/challenges/a-very-big-sum/problem
+
+#!/bin/python3
+
+import sys
+
+def aVeryBigSum(n, ar):
+    return sum(ar)
+
+n = int(input().strip())
+ar = list(map(int, input().strip().split(' ')))
+result = aVeryBigSum(n, ar)
+print(result)
