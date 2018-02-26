869395c1a485b6f9453709a58d4bab06c73b3355
Samuel Barrera <sbarrera@empresas.security.cl>
2018-02-26 16:50:15 -0300

    Mas desafios

--- Python - Language Proficiency/E025 - String Formatting/ejercicio.py
+++ Python - Language Proficiency/E025 - String Formatting/ejercicio.py
@@ -2,8 +2,19 @@
 # >>> https://www.hackerrank.com/challenges/python-string-formatting/problem
 
 def print_formatted(number):
-    # your code goes here
+    sizeBinary = len('{0:b}'.format(number))
+    for x in range(number):
+        value = x + 1
+        decimalText = str(value)
+        decimalText = ' '*(sizeBinary - len(decimalText)) + decimalText
+        octalText = '{0:o}'.format(value)
+        octalText = ' '*(sizeBinary - len(octalText)) + octalText
+        hexaText = '{0:x}'.format(value)
+        hexaText = ' '*(sizeBinary - len(hexaText)) + hexaText
+        binaryText = '{0:b}'.format(value)
+        binaryText = ' '*(sizeBinary - len(binaryText)) + binaryText
+        print('{} {} {} {}'.format(decimalText, octalText, hexaText.upper(), binaryText))
 
 if __name__ == '__main__':
-    n = 17 #int(raw_input())
+    n = 8 #int(raw_input())
     print_formatted(n)
