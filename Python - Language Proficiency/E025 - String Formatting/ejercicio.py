301c7b60f521e46387dff759b11b1355846ff2ca
Samuel Barrera <sbarrera@empresas.security.cl>
2018-02-26 16:00:13 -0300

    Mas desafios

---
+++ Python - Language Proficiency/E025 - String Formatting/ejercicio.py
@@ -0,0 +1,9 @@
+# >>> String Formatting
+# >>> https://www.hackerrank.com/challenges/python-string-formatting/problem
+
+def print_formatted(number):
+    # your code goes here
+
+if __name__ == '__main__':
+    n = 17 #int(raw_input())
+    print_formatted(n)
