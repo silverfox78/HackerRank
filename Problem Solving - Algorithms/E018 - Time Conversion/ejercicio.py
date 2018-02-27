e1026fecc7f83d538fc9f566a4c7a85ea4ae265e
Samuel Barrera <sbarrera@gmail.com>
2018-02-26 21:50:01 -0300

    Mas desafios

---
+++ Problem Solving - Algorithms/E018 - Time Conversion/ejercicio.py
@@ -0,0 +1,13 @@
+# >>> Time Conversion
+# >>> https://www.hackerrank.com/challenges/time-conversion/problem
+
+#!/bin/python3
+
+import sys
+
+def timeConversion(s):
+    # Complete this function
+
+s = input().strip()
+result = timeConversion(s)
+print(result)
