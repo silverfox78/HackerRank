e71362a5b399bc80302adb7e8e8b9d1cd967d4c7
Samuel Barrera <sbarrera@empresas.security.cl>
2018-03-01 08:12:54 -0300

    Mas desafios

--- 30 Days of code/Day 21 - Generics/ejercicio.cs
+++ 30 Days of code/Day 21 - Generics/ejercicio.cs
@@ -1,2 +1,29 @@
 // >>> Day 21: Generics
 // >>> https://www.hackerrank.com/challenges/30-generics/problem
+
+using System;
+
+class Printer
+{
+
+    
+    static void Main(string[] args)
+	{
+		int n = Convert.ToInt32(Console.ReadLine());
+		int[] intArray = new int[n];
+		for (int i = 0; i < n; i++)
+		{
+			intArray[i] = Convert.ToInt32(Console.ReadLine());
+		}
+		
+		n = Convert.ToInt32(Console.ReadLine());
+		string[] stringArray = new string[n];
+		for (int i = 0; i < n; i++)
+		{
+			stringArray[i] = Console.ReadLine();
+		}
+		
+		PrintArray<Int32>(intArray);
+		PrintArray<String>(stringArray);
+	}
+}
