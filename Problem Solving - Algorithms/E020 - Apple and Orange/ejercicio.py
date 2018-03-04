462ff1b2f371a1c3d227fc2f418fea0fcb137dfa
Samuel Barrera <sbarrera@gmail.com>
2018-03-04 17:47:41 -0300

    Mas desafios

--- Problem Solving - Algorithms/E020 - Apple and Orange/ejercicio.py
+++ Problem Solving - Algorithms/E020 - Apple and Orange/ejercicio.py
@@ -2,3 +2,29 @@
 # >>> https://www.hackerrank.com/challenges/apple-and-orange/problem
 
 
+#!/bin/python3
+
+import sys
+
+def countApplesAndOranges(s, t, a, b, apples, oranges):
+    #print('s:{} - t:{} - a:{} - b:{} - apples:{} - oranges:{}'.format(s, t, a, b, apples, oranges))
+    countApples = 0
+    countOrange = 0
+    for x in apples:
+        if s <= (a + x) <= t:
+            countApples += 1
+    for x in oranges:
+        if s <= (b + x) <= t:
+            countOrange += 1
+    print('{}\n{}'.format(countApples, countOrange))
+
+if __name__ == "__main__":
+    s, t = input().strip().split(' ')
+    s, t = [int(s), int(t)]
+    a, b = input().strip().split(' ')
+    a, b = [int(a), int(b)]
+    m, n = input().strip().split(' ')
+    m, n = [int(m), int(n)]
+    apple = list(map(int, input().strip().split(' ')))
+    orange = list(map(int, input().strip().split(' ')))
+    countApplesAndOranges(s, t, a, b, apple, orange)
