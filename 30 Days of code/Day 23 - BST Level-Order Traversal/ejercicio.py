fea90f68def50a572c85cae55e54b7584fb3d385
Samuel Barrera <sbarrera@gmail.com>
2018-03-04 00:55:19 -0300

    Mas desafios

--- 30 Days of code/Day 23 - BST Level-Order Traversal/ejercicio.py
+++ 30 Days of code/Day 23 - BST Level-Order Traversal/ejercicio.py
@@ -23,11 +23,50 @@ class Solution:
 
 # ----------------------------------------------------------------------------
 
+    def countLeavel(self, node, size):
+        maximo = size
+        if node.left == None and node.right == None:
+            return maximo
+        else:
+            if node.left != None:
+                izq = self.countLeavel(node.left, size + 1)
+                if izq > maximo:
+                    maximo = izq
+            if node.right != None:
+                der = self.countLeavel(node.right, size + 1)
+                if der > maximo:
+                    maximo = der
+        return maximo
+
+    def printLeavel(self, node, vector, busca, nivel):
+        array = vector
+        if busca == nivel:
+            if node.left != None:
+                array.append(node.left.data)
+            if node.right != None:
+                array.append(node.right.data)
+
+        if node.left != None:
+            array = self.printLeavel(node.left, array, busca, nivel + 1)
+        if node.right != None:
+            array = self.printLeavel(node.right, array, busca, nivel + 1)
+        return array
+
     def levelOrder(self,root):
-        #Write your code here
+        size = 0
+        salida = ''
+        if root != None:
+            size = self.countLeavel(root, size)
 
-
-
+        if root != None:
+            for x in range(size + 1):
+                array = []
+                if x == 0:
+                    array.append(root.data)
+                else:
+                    array = self.printLeavel(root, array, x, 1)
+            salida += ' ' + ' '.join([str(x) for x in array])
+        print(salida.strip())
 
 # ----------------------------------------------------------------------------
 
@@ -38,3 +77,9 @@ for i in range(T):
     data=int(input())
     root=myTree.insert(root,data)
 myTree.levelOrder(root)
+
+
+# 25 12 39 9 19 23 31 55 35 41 60 70 90
+# 25 12 39 9 19 31 55 23 35 41 60 70 90
+# 25 12 39 9 19 31 55 23 35 41 60 70 90
+
