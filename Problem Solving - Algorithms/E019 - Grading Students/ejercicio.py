8a6100a1e84e3e9a65e2b56d4133b38bf9866803
Samuel Barrera <sbarrera@gmail.com>
2018-03-04 11:58:04 -0300

    Mas desafios

--- Problem Solving - Algorithms/E019 - Grading Students/ejercicio.py
+++ Problem Solving - Algorithms/E019 - Grading Students/ejercicio.py
@@ -1,3 +1,30 @@
 # >>> Grading Students
 # >>> https://www.hackerrank.com/challenges/grading/problem
 
+#!/bin/python3
+
+import sys
+
+def solve(grades):
+    array = []
+    for x in grades:
+        decimal = int(x / 10) * 10
+        diference = x - decimal
+        add = 5 if diference < 5 else 10
+        evalvalue = decimal + add
+        diferenceFinal = evalvalue - x
+        if diferenceFinal < 3:
+            evalvalueFinal = evalvalue if evalvalue >= 40 else x
+        else:
+            evalvalueFinal = x
+        array.append(evalvalueFinal)
+    return array
+
+n = int(input().strip())
+grades = []
+grades_i = 0
+for grades_i in range(n):
+   grades_t = int(input().strip())
+   grades.append(grades_t)
+result = solve(grades)
+print ("\n".join(map(str, result)))
