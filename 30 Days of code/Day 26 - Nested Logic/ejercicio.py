cf19f0508580789c20b317ba140a4686de9561ac
Samuel Barrera <sbarrera@gmail.com>
2018-03-11 17:02:57 -0300

    Mas desafios

--- 30 Days of code/Day 26 - Nested Logic/ejercicio.py
+++ 30 Days of code/Day 26 - Nested Logic/ejercicio.py
@@ -2,26 +2,20 @@
 # >>> https://www.hackerrank.com/challenges/30-nested-logic/problem
 
 #!/bin/python3
-
-from datetime import date, time, datetime
 import sys
 
 
 if __name__ == "__main__":
     date01 = list(map(int, input().strip().split(' ')))
-    date02 = list(map(int, input().strip().split(' ')))
-    realdate01 = datetime.strptime('{}-{}-{}'.format(date01[2], date01[1], date01[0]), '%Y-%m-%d')
-    realdate02 = datetime.strptime('{}-{}-{}'.format(date02[2], date02[1], date02[0]), '%Y-%m-%d')
-    delta = realdate01 - realdate02
-    days = delta.days
+    date02 = list(map(int, input().strip().split(' ')))   
 
-    if days <= 0:
-        print(0)
+    if date01[2] > date02[2]:
+        print(10000)
     else:
-        if date01[2] == date02[2] and date01[1] == date02[1]:
-            print(days * 15)
+        if date01[1] > date02[1] and date01[2] == date02[2]:
+            print(500 * abs(date01[1] - date02[1]))
         else:
-            if date01[2] == date02[2]:
-                print(500 * abs(date01[1] - date02[1]))
-            else:   
-                print(10000)
+            if date01[0] > date02[0] and date01[1] == date02[1] and date01[2] == date02[2]:
+                print(15 * abs(date01[0] - date02[0]))
+            else:
+                print(0)
