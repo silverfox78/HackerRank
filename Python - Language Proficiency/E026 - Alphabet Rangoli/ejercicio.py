ba5ae90897a13563815f264988032c3d300cc4ab
Samuel Barrera <sbarrera@gmail.com>
2018-03-12 21:32:29 -0300

    Mas desafios

--- Python - Language Proficiency/E026 - Alphabet Rangoli/ejercicio.py
+++ Python - Language Proficiency/E026 - Alphabet Rangoli/ejercicio.py
@@ -1,9 +1,25 @@
 # >>> Alphabet Rangoli
 # >>> https://www.hackerrank.com/challenges/alphabet-rangoli/problem
 
+def printLine(number, large):
+    lineNumber = int(((large - 1) / 4) + 1) - number
+    letterNumber = 97 + number
+    print('{} {} - {}'.format(str(chr(letterNumber)), lineNumber, number))
+
 def print_rangoli(size):
-    # your code goes here
+    large = ((size - 1) * 4) + 1
+    print(large)
+    for x in range(size -1, -1, -1):
+        #print(str(chr(97 + x)))
+        printLine(x, large)
+    for x in range(1, size, 1):
+        printLine(x, large)
+
 
 if __name__ == '__main__':
-    n = int(input())
+    #n = int(input())
+    n = 5
     print_rangoli(n)
+    print('{} {} {}'.format(*[1, 2, 3])))
+
+
