288c970cc0fc16ae78497da27717571734badc73
Samuel Barrera <sbarrera@gmail.com>
2018-03-12 22:14:32 -0300

    Mas desafios

--- Python - Language Proficiency/E026 - Alphabet Rangoli/ejercicio.py
+++ Python - Language Proficiency/E026 - Alphabet Rangoli/ejercicio.py
@@ -2,24 +2,26 @@
 # >>> https://www.hackerrank.com/challenges/alphabet-rangoli/problem
 
 def printLine(number, large):
-    lineNumber = int(((large - 1) / 4) + 1) - number
-    letterNumber = 97 + number
-    print('{} {} - {}'.format(str(chr(letterNumber)), lineNumber, number))
+    return ('-'*(int((large - (number*4) - 1) / 2))) + ('{}' if number == 0 else '{}-'*number*2 + '{}') + ('-'*(int((large - (number*4) - 1) / 2)))
 
 def print_rangoli(size):
     large = ((size - 1) * 4) + 1
-    print(large)
     for x in range(size -1, -1, -1):
-        #print(str(chr(97 + x)))
-        printLine(x, large)
+        array = [chr(97 + x) for x in range(size - 1, x, -1)]
+        array.extend([chr(97 + x) for x in range(x, size, 1)])
+        formatLine = printLine(int(len(array) / 2), large)
+        print(formatLine.format(*array))
     for x in range(1, size, 1):
-        printLine(x, large)
+        array = [chr(97 + x) for x in range(size - 1, x, -1)]
+        array.extend([chr(97 + x) for x in range(x, size, 1)])
+        formatLine = printLine(int(len(array) / 2), large)
+        print(formatLine.format(*array))
 
 
 if __name__ == '__main__':
     #n = int(input())
-    n = 5
+    n = 10
     print_rangoli(n)
-    print('{} {} {}'.format(*[1, 2, 3])))
+
 
 
