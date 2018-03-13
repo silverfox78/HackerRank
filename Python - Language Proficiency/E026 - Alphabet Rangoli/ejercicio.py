3fa44573ce57e342d98b163e7f8283366d12cedd
Samuel Barrera <sbarrera@gmail.com>
2018-03-12 21:10:16 -0300

    Mas desafios

---
+++ Python - Language Proficiency/E026 - Alphabet Rangoli/ejercicio.py
@@ -0,0 +1,9 @@
+# >>> Alphabet Rangoli
+# >>> https://www.hackerrank.com/challenges/alphabet-rangoli/problem
+
+def print_rangoli(size):
+    # your code goes here
+
+if __name__ == '__main__':
+    n = int(input())
+    print_rangoli(n)
