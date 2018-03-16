7e2ef06ff8bf7aacf4ebbfc5b4b371180a018368
Samuel Barrera <sbarrera@gmail.com>
2018-03-16 00:16:04 -0300

    Mas desafios

--- Python - Language Proficiency/E027 - Capitalize/ejercicio.py
+++ Python - Language Proficiency/E027 - Capitalize/ejercicio.py
@@ -1,2 +1,10 @@
 # >>> Capitalize
-# >>> 
+# >>> https://www.hackerrank.com/challenges/capitalize/problem
+
+def capitalize(string):
+    return ' '.join([x.capitalize() for x in string.split(' ')])
+
+if __name__ == '__main__':
+    string = 'chris alan' #input()
+    capitalized_string = capitalize(string)
+    print(capitalized_string)
