3d213f5de796409b39e8c586f1f7a31d28f2b722
Samuel Barrera <sbarrera@gmail.com>
2018-03-18 00:07:03 -0300

    Mas desafios

--- Python - Language Proficiency/E028 - The Minion Game/ejercicio.py
+++ Python - Language Proficiency/E028 - The Minion Game/ejercicio.py
@@ -1,3 +1,15 @@
 # >>> The Minion Game
 # >>> https://www.hackerrank.com/challenges/the-minion-game/problem
 
+import itertools
+
+def minion_game(string):
+    array = []
+    for i in range(len(string)):
+        els = [''.join(list(x)) for x in itertools.combinations(string, i)]
+        array.extend(els)
+    print(array)
+
+if __name__ == '__main__':
+    s = 'BANANA' #input()
+    minion_game(s)
