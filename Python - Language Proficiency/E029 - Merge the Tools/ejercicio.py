aa60608693797e82cf9067485a7495080d787dea
Samuel Barrera <sbarrera@gmail.com>
2018-03-18 21:05:20 -0300

    Mas desafios

---
+++ Python - Language Proficiency/E029 - Merge the Tools/ejercicio.py
@@ -0,0 +1,10 @@
+# >>> Merge the Tools!
+# >>> https://www.hackerrank.com/challenges/merge-the-tools/problem
+
+def merge_the_tools(string, k):
+    # your code goes here
+
+
+if __name__ == '__main__':
+    string, k = input(), int(input())
+    merge_the_tools(string, k)
